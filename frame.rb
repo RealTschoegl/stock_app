0a5ebb7759c092a4d164b9b57edd7792fb146e66
RealTschoegl <Matt.Tschoegl@gmail.com>
2013-06-21 12:40:55 -0500

    Added class and altered addition to compensate

--- frame.rb
+++ frame.rb
@@ -1,52 +1,82 @@
-@companies = {"GOOG" => 0, "MSFT" => 0, "YHOO" => 0, "FB" => 0}
-@free_cash_flow = { :GOOG => 16500, :MSFT => 31626, :YHOO => -281, :FB =>  1612 }
-cash_on_hand = { :GOOG => 50100, :MSFT => 73790, :YHOO => 3010, :FB => 15103 }
-debt = { :GOOG => 22083, :MSFT => 54908, :YHOO => 2543, :FB => 2260 }
-assets = { :GOOG => 93798, :MSFT => 121271, :YHOO => 17108, :FB => 3348 }
-num_shares = { :GOOG => 332, :MSFT => 8350, :YHOO => 1008, :FB => 2420 }
 
-@stock_prices = {}
+class Company
 
-	def collective
-		assign_hash
+@@companies = {
+	"GOOG" => {
+		:free_cash_flow => 16500,
+		:cash_on_hand => 50100,
+		:num_shares => 332, 
+		:expected_share_value => 0
+		},
+	"MSFT" => {
+		:free_cash_flow => 31626,
+		:cash_on_hand => 73790,
+		:num_shares => 8350,
+		:expected_share_value => 0
+		},
+	"YHOO" => {
+		:free_cash_flow => -281,
+		:cash_on_hand => 3010,
+		:num_shares => 1008,
+		:expected_share_value => 0
+		},
+	"FB" => {
+		:free_cash_flow => 1612,
+		:cash_on_hand => 15103,
+		:num_shares => 2420,
+		:expected_share_value => 0
+		}
+}
+
+	def self.aggregator
 		add_cash_flow
-		puts @stock_prices
+		add_cash_on_hand
+		num_shares
 	end
 	
-	def assign_hash
-		@companies.each do |company, value|
-			@stock_prices[company] = value 
+
+	def self.add_cash_flow
+		@@companies.map do |company, value|
+			value[:expected_share_value] += value[:free_cash_flow]
 		end
 	end
-
-	def add_cash_flow
-		@free_cash_flow.each do |company, value|
-			if @stock_prices.has_key?(company)
-				@stock_prices[company] += value
-			else
-			end
+	
+	def self.add_cash_on_hand
+		@@companies.map do |company, value|
+			value[:expected_share_value] += value[:cash_on_hand]
 		end
 	end
-
-	def cash_on_hand
-
+	
+	def self.num_shares
+		@@companies.map do |company, value|
+			value[:expected_share_value] = (value[:expected_share_value].to_f/value[:num_shares].to_f).round(2)
+		end
 	end
 	
-	def time_value_of_money
-		present value = future value / (1 + interest rate)^number of years
+	def self.assignment_test
+		@@companies.each do |company, value|
+			puts company, value[:free_cash_flow], value[:cash_on_hand]
+		end
 	end
+	
+	#def time_value_of_money
+	#	present value = future value / (1 + interest rate)^number of years
+	#end
 
 	
-	def interest_rate
-		puts "What do you think the market will grow at?"
-		growth_rate = gets.chomp.to_f
-	end	
-	
-	def years
-		puts "How long do you think this will continue for?  10 years? 20 years? 30 years?"
-		years_estimate = gets.chomp.to_f
-	end
-	
-	def num_shares
+	#def interest_rate
+	#	puts "What do you think the market will grow at?"
+	#	growth_rate = gets.chomp.to_f
+	#end	
 
-	end
+	#def growth_rate
+	#	puts "Do you think this sector will grow faster than other sectors?"
+	#	growth_rate = gets.chomp.to_f
+	#end
+	
+	#def years
+	#	puts "How long do you think this will continue for?  10 years? 20 years? 30 years?"
+	#	years_estimate = gets.chomp.to_f
+	#end
+	
+end
