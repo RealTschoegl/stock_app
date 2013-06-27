c26ff7e4a24bcd250a92187ea768e8a3af92f5d5
RealTschoegl <Matt.Tschoegl@gmail.com>
2013-06-27 18:26:54 -0500

    Added new valuation methods and improved model

--- frame.rb
+++ frame.rb
@@ -1,40 +1,67 @@
-
 class Company
+
+@risk_free_rate = .0141 #5 year treasury yield at market
+@market_growth_rate = .2038 #S&P500 1 year growth rate
 
 @@companies = {
 	"GOOG" => {
 		:free_cash_flow => 16500,
 		:cash_on_hand => 50100,
 		:num_shares => 332, 
-		:expected_share_value => 0
+		:PE_ratio => 25.8,
+		:beta => 1.19,
+		:expected_share_value => 0,
+		:composite_share_value => 0
 		},
 	"MSFT" => {
 		:free_cash_flow => 31626,
 		:cash_on_hand => 73790,
 		:num_shares => 8350,
-		:expected_share_value => 0
+		:PE_ratio => 17.5,
+		:beta => 1.10,
+		:expected_share_value => 0, 
+		:composite_share_value => 0
 		},
 	"YHOO" => {
 		:free_cash_flow => -281,
 		:cash_on_hand => 3010,
 		:num_shares => 1008,
-		:expected_share_value => 0
+		:PE_ratio => 6.67,
+		:beta => .89,
+		:expected_share_value => 0,
+		:composite_share_value => 0
 		},
 	"FB" => {
 		:free_cash_flow => 1612,
 		:cash_on_hand => 15103,
 		:num_shares => 2420,
-		:expected_share_value => 0
+		:PE_ratio => 872,
+		:beta => 1.33,
+		:expected_share_value => 0,
+		:composite_share_value => 0
 		}
 }
+	def self.composite_valuation
+		puts (free_cash_flow_method + divided_discount_model + capm_method)/3
+	end
 
-	def self.aggregator
+	def self.free_cash_flow_method
 		add_cash_flow
 		add_cash_on_hand
 		time_value_of_money
 		num_shares
 	end
 	
+	# def self.dividend_discount_model
+		# Value = Dividend per share / (Discount rate - dividend growth rate)
+	# end
+	
+	def self.capm_method
+		@@companies.map do |company, value|
+			rate_of_return = @risk_free_rate + value[:beta](@market_growth_rate - @risk_free_rate)
+			value[:composite_share_value] = (rate_of_return + 1) * years * current_stockprice
+		end
+	end
 
 	def self.add_cash_flow
 		@@companies.map do |company, value|
@@ -87,7 +114,7 @@ class Company
 	end
 	
 	def self.years
-		puts "How long do you think this will continue for?  10 years? 20 years? 30 years?"
+		puts "How many years do you want to hold the stock for?"
 		gets.chomp.to_f
 	end
 	
